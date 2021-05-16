8176d50b1651d8379e657631c5938a795501122a
manuel-192 <manuel-192@outlook.com>
2021-05-16 18:06:56 +0300

    .

---
+++ PKGBUILDs/cd.bash/cd.bash
@@ -0,0 +1,199 @@
+#!/bin/bash
+
+# A 'cd' designed for CDPATH.
+#
+# Can specify folders to be excludes using variables:
+#   - CDPATH_EXCLUDE      array of (absolute and relative) folders
+#   - CDPATH_EXCLUDE_FILES files (likely an empty file) in a folder
+
+_cd_out() {
+    if [ "$CDOUT" = "all" ] ; then
+        if [ -z "$2" ] ; then
+            echo "==> $1" >&2
+        else
+            echo "    $1" >&2
+        fi
+    fi
+}
+
+_cd_out_important() {
+    local old_cdout="$CDOUT"
+    CDOUT=all
+    _cd_out "$@"
+    CDOUT="$old_cdout"
+}
+
+_cd_is_unique_value() {
+    local value="$1"
+    local xx
+    shift
+
+    for xx in "$@" ; do
+        if [ "$value" = "$xx" ] ; then
+            return 1
+        fi
+    done
+    return 0
+}
+
+_cd_builtin_cd() {
+    builtin cd $opts "$@" || _cd_out_important "failed: cd $opts $*"
+}
+
+cd() {
+    local arg
+    local newdir=""
+    local realpath=/usr/bin/realpath
+    local conf=$HOME/.config/cd.conf
+    local opts=""
+
+    if [ ! -r $conf ] ; then
+        if [ -r /etc/skel/cd.conf ] ; then
+            _cd_out_important "Copying /etc/skel/cd.conf to $conf."
+            _cd_out_important "Please make sure $conf contains useful settings!"
+            cp /etc/skel/cd.conf $HOME/.config/
+        else
+            _cd_out_important "You need to have a proper config file $conf!"
+            _cd_out_important "/etc/skel/cd.conf was not found either."
+        fi
+        return 1
+        # RunInTerminal nano $HOME/.config/cd.conf
+    fi
+    if [ -z "CDPATH_ARR" ] && [ -z "$CDPATH_EXCLUDE" ] ; then
+        source $conf || return 1
+        export CDPATH="$(echo "${CDPATH_ARR[@]}" | tr ' ' ':')"
+    fi
+
+    case "$CDOUT" in
+        all | important) ;;
+        *) _cd_out_important "value of CDOUT in file $conf is unsupported!" ; return 1 ;;
+    esac
+
+    # handle options
+    for arg in "$@" ; do
+        case "$arg" in
+            -)
+                if [ -n "$newdir" ] ; then
+                    _cd_out_important "please give only one target dir"
+                    return 1
+                fi
+                newdir="$arg"
+                ;;
+            -L | -P | -e | "-@")
+                opts+=" $arg"
+                ;;
+            -*) _cd_out_important "unsupported option $arg" ; return 1 ;;
+            *)
+                if [ -n "$newdir" ] ; then
+                    _cd_out_important "please give only one target dir"
+                    return 1
+                fi
+                newdir="$arg"                                       # this is where we want to go!
+                [ "${newdir: -1}" = "/" ] && newdir=${newdir:: -1}  # remove possible trailing slash
+                ;;
+        esac
+    done
+
+    local dir dirs=() ix dir2 file
+    local newdir_excluded_by=()
+    local excluded_dir="" excluded
+
+    # First handle some simple cases (note: these allow an excluded path!)
+    case "$newdir" in
+        - | "")
+            _cd_out "special paths"
+            _cd_builtin_cd $newdir
+            return
+            ;;
+        /*)
+            if [ -d "$newdir" ] ; then
+                _cd_out "accept all existing absolute paths"
+                _cd_builtin_cd "$newdir"
+                return
+            fi
+            ;;
+        *)
+            if [ -d ./"$newdir" ] ; then
+                _cd_out "accept all existing relative paths"
+                _cd_builtin_cd ./"$newdir"
+                return
+            fi
+            ;;
+    esac
+
+    # Now we'll search for CDPATH and CDPATH_EXCLUDE.
+    # $newdir
+    #  - is not empty
+    #  - is a relative path
+    #  - has no trailing slash
+
+    # Find all matches that are not excluded
+    for dir in "${CDPATH_ARR[@]}" ; do
+        if [ -d "$dir/$newdir" ] ; then
+            excluded_dir=""
+            for dir2 in "${CDPATH_EXCLUDE[@]}" ; do
+                if [ "$dir2" = "$newdir" ] || [ "$dir2" -ef "$dir/$newdir" ] || [ "$dir2" -ef "$newdir" ] ; then
+                    excluded_dir="$dir2"
+                    if (_cd_is_unique_value "$excluded_dir" "${newdir_excluded_by[@]}") ; then
+                        newdir_excluded_by+=("$excluded_dir")           # exclude absolute or relative path
+                    fi
+                    break
+                fi
+            done
+            if [ -z "$excluded_dir" ] ; then
+                excluded=no
+                for file in "${CDPATH_EXCLUDE_FILES[@]}" ; do
+                    if [ -r "$dir/$newdir/$file" ] ; then
+                        newdir_excluded_by+=("$dir/$newdir/$file")
+                        excluded=yes
+                        break
+                    fi
+                done
+                if [ "$excluded" = "no" ] ; then
+                    dirs+=("$dir/$newdir")
+                fi
+            fi
+        fi
+    done
+
+    # Now we have all possible matches in $dirs.
+    # Do final touch.
+
+    case "${#dirs[@]}" in
+        1)  _cd_out "one match"
+            _cd_builtin_cd "${dirs[0]}"
+            ;;
+        0)  if [ -n "$newdir_excluded_by" ] ; then
+                _cd_out_important "excluded by '${newdir_excluded_by[*]}'"
+            else
+                _cd_out_important "'$newdir': no match found in CDPATH_ARR."
+            fi
+            ;;
+        *)
+            # Many matches, round robin them.
+            # If $PWD matches, go to the next match!
+            for ((ix=0; ix < ${#dirs[@]}; ix++)) ; do
+                dir="${dirs[$ix]}"
+                if [ "$dir" -ef "$PWD" ]; then
+                    ((ix++))
+                    [ $ix = ${#dirs[@]} ] && ix=0
+                    _cd_out "one of many matches"
+                    _cd_builtin_cd "${dirs[$ix]}"
+                    return
+                fi
+            done
+
+            # Go to the first of many matches. Inform about more matches!
+            _cd_out_important "${#dirs[@]} matches (run again for the next):"
+            for ((ix=0; ix < ${#dirs[@]}; ix++)) ; do
+                dir="${dirs[$ix]}"
+                case "$dir" in
+                    "$HOME"*) dir="~${dir:${#HOME}}" ;;
+                esac
+                _cd_out_important "$dir" no
+            done
+            _cd_out "first of many matches:"
+            _cd_builtin_cd "${dirs[0]}"
+            ;;
+    esac
+}
---
+++ PKGBUILDs/cd.bash/cd.bash.md
@@ -0,0 +1,169 @@
+# cd.bash
+
+Another enhanced `cd` replacement, implemented as a bash *function*.
+
+Works like traditional builtin `cd`, but enhances the usage of the `CDPATH` variable with ways to *exclude* certain folders found by the `CDPATH` feature.<br>
+Otherwise function `cd` supports the same options and features as the builtin `cd` command.
+
+# Synopsis
+```
+cd [options] folder
+```
+
+where
+Item | Description
+:--- | :---
+options | Options for the builtin `cd`.
+folder | Folder specification like with the builtin `cd`.
+
+<br>
+
+# Installation and further preparations
+
+After installing package `cd.bash` you need to write the following lines to your `~/.bashrc` (most likely in the end of that file):
+```
+source $HOME/.config/cd.conf
+source /etc/skel/cd.bash
+```
+and then copy file `/etc/skel/cd.conf` into your `$HOME/.config` folder:
+```
+cp /etc/skel/cd.conf ~/.config/
+```
+
+After the above, modify the configuration file `~/.config/cd.conf` containing the values for variables:
+
+Variable | Description
+:--- | :---
+CDPATH_ARR | Same as CDPATH but as an array.
+CDPATH_EXCLUDE | Similar array but defines (absolute or relative) paths to be excluded.
+CDPATH_EXCLUDE_FILES | Names of files that make the folder excluded (example value: `.no-cd`).
+CDOUT | Verbosity option. Supported values: `important` (default), and `all` (for additional debug info).
+
+Note that it is best to select carefully the values of the arrays, otherwise `cd` may not find the folders you are trying to go to or avoid.<br>
+See also the examples below.
+
+<br>
+
+## Adding bash completion support
+
+You can make bash completion for the `cd` command to take folder exclusions into account by
+modifying file `/usr/share/bash-completion/bash_completion` as follows:
+
+### From (lines 1829..1838)
+```
+# we have a CDPATH, so loop on its contents
+for i in ${CDPATH//:/$'\n'}; do
+    # create an array of matched subdirs
+    k="${#COMPREPLY[@]}"
+    for j in $(compgen -d -- $i/$cur); do
+        if [[ ($mark_symdirs && -L $j || $mark_dirs && ! -L $j) && ! -d ${j#$i/} ]]; then
+            j+="/"
+        fi
+        COMPREPLY[k++]=${j#$i/}
+    done
+done
+```
+#### to
+```
+# we have a CDPATH, so loop on its contents
+# we also have arrays CDPATH_EXCLUDE and CDPATH_EXCLUDE_FILES, so take them into account too
+local exclude xx
+for i in ${CDPATH//:/$'\n'}; do
+    # create an array of matched subdirs
+    k="${#COMPREPLY[@]}"
+    for j in $(compgen -d -- $i/$cur); do
+        if [ -n "$CDPATH_EXCLUDE" ]; then
+            exclude=no
+            for xx in "${CDPATH_EXCLUDE[@]}"; do
+                if [ "$j" = "$xx" ] || [ "$j" -ef "$i/$xx" ] || [ "$j" -ef "$xx" ]; then
+                    exclude=yes
+                    break
+                fi
+            done
+            [ "$exclude" = "yes" ] && continue
+            for xx in "${CDPATH_EXCLUDE_FILES[@]}"; do
+                if [ -r "$j/$xx" ]; then
+                    exclude=yes
+                    break
+                fi
+            done
+            [ "$exclude" = "yes" ] && continue
+        fi
+        if [[ ($mark_symdirs && -L $j || $mark_dirs && ! -L $j) && ! -d ${j#$i/} ]]; then
+            j+="/"
+        fi
+        COMPREPLY[k++]=${j#$i/}
+    done
+done
+```
+
+<br>
+
+# Examples of usage
+
+## Basic examples
+
+Command | What happens
+:--- | :---
+`cd` | Go $HOME
+`cd -` | Go the the previous folder.
+`cd ..` | Go to the parent folder.
+
+<br>
+
+## Examples assuming configured variables in `~/.config/cd.conf`
+
+Assume the following folders exist:
+```
+$HOME/Documents/Work
+$HOME/Documents/Family/Maggie
+$HOME/Documents/Family/Homer
+$HOME/Documents/Family/Children
+$HOME/Documents/Family/Children/Lisa/School
+$HOME/Documents/Family/Children/Lisa/Private
+$HOME/Documents/Family/Children/Bart/School
+$HOME/Documents/Family/Children/Bart/Secret
+$HOME/Documents/Family/Children/Bart/Foobar
+```
+and this file exists:
+```
+$HOME/Documents/Family/Children/Lisa/Private/.no-cd
+```
+
+With the following values of variables:
+
+```
+CDPATH_ARR=(
+    $HOME/Documents
+    $HOME/Documents/Family/Maggie
+    $HOME/Documents/Family/Homer
+    $HOME/Documents/Family/Children/Lisa
+    $HOME/Documents/Family/Children/Bart
+)
+CDPATH_EXCLUDE=(
+    $HOME/Documents/Family/Children/Bart/Secret
+    $HOME/Documents/Family/Children/Lisa/Private
+    Foobar
+)
+CDPATH_EXCLUDE_FILES=(.no-cd)
+CDOUT=important
+```
+
+e.g. the following commands could be run:
+
+Command | What happens
+:--- | :---
+`cd Work` | Go to `$HOME/Documents/Work`
+`cd School` | Goes to the first matched folder, and shows information about the other matches.<br>Running the same command again will go to the next match.
+`cd Foobar` | Nothing, because excluded by `CDPATH_EXCLUDE` value `Foobar`.<br>A small notification will explain the reason.
+`cd Private` | Nothing, because excluded by `$CDPATH_EXCLUDE_FILE` in that folder.<br>A small notification will explain the reason.
+`cd Bart ; cd Foobar` | First goes to `Bart`, then to `Foobar` even though Foobar was excluded above! See Note 2 below.
+
+<br>
+
+# Notes
+
+1. If you have added the bash completion support as mentioned above, you can use bash completion (press the `tab` key to complete the word) while writing the folder names.
+2. The `cd` function can go to given (and existing) absolute path even though it is excluded. The same is true for a *direct* relative path.<br>
+3. If you have an existing CDPATH value, it will be overwritten by the config file (by default).
+4. To make sure all CDPATH related variables get their proper values, it is recommended to re-login into the machine, or simply reboot.<br>The *minimum* is to start a new terminal instance because bash completion may not work with existing terminal instances.
