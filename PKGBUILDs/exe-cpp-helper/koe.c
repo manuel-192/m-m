e525dd71196033b2bc905c7b108029da037888e1
manuel-192 <manuel-192@outlook.com>
2022-11-20 13:05:07 +0200

    .

---
+++ PKGBUILDs/exe-cpp-helper/koe.c
@@ -0,0 +1,7 @@
+#!/usr/bin/exe-clang
+
+#include <stdio.h>
+
+int main() {
+  printf ("hello world\n");
+}
---
+++ PKGBUILDs/exe-cpp-helper/koe.cpp
@@ -0,0 +1,9 @@
+#!/usr/bin/exe-clang++
+
+#include <iostream>
+
+int main() {
+  using namespace std;
+  cout << "hello c++ world" << endl;
+  return 0;
+}
