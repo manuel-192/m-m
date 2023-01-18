3bc2ac052bc2e5e494a79a4f4947ed13729bb1f6
manuel-192 <manuel-192@outlook.com>
2023-01-18 11:49:26 +0200

    .

--- PKGBUILDs/vb-notif/virtualbox-ext-oracle.bash
+++ PKGBUILDs/vb-notif/virtualbox-ext-oracle.bash
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-Pkgver() { pacman -Q "$1" | awk '{print $NF}' | cut -d'-' -f1 ; }
+Pkgver() { pacman -Q "$1" 2>/dev/null | awk '{print $NF}' | cut -d'-' -f1 ; }
 
 Main()
 {
     local extensions vb
 
     extensions=$(Pkgver virtualbox-ext-oracle)
-    [ -n "$extensions" ] || return
+    [ -n "$extensions" ] || return 0
 
     vb=$(Pkgver virtualbox)
-    [ -n "$vb" ] || return
+    [ -n "$vb" ] || return 0
 
     if [ "$vb" != "$extensions" ] ; then
         printf "\n==> You probably should run command: yay -Sua virtualbox-ext-oracle\n\n"
